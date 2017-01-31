f9c71ec15073ed5ea76740dcf11898690dce8c9a
Dave Hubbard <dave.hubbard@gmail.com>
2017-01-31 22:32:18 +0000

    add basic DB class

---
+++ lib/DB.php
@@ -0,0 +1,22 @@
+<?php
+namespace FSPA;
+
+# DB class
+class DB {
+    public static $dbh = null;
+    public $connected = false;
+    public $error = null;
+
+    public function __construct() {
+        try {
+            $this->dbh = new \PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, [ \PDO::ATTR_PERSISTENT => DB_USE_PERSISTENT ]);
+            $this->dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->connected = true;
+        }
+        catch (PDOException $e) {
+            $this->connected = false;
+            $this->error = $e->getMessage();
+        }
+    }
+}
+?>
---
+++ lib/startup.php
@@ -0,0 +1,9 @@
+<?php
+
+# Load constants
+require_once('const.php');
+
+# Load DB class
+require_once('DB.php');
+
+?>
